763576f3bd1c3d70ffb2ab17d8fe4c778c20dd8d
dominicsieli <dominicsieli@gmail.com>
2021-09-04 01:25:05 -0700

    Updated Hash_Table.h

--- Source/Hash_Table.h
+++ Source/Hash_Table.h
@@ -1,89 +1,91 @@
 #pragma once
-
 #include <list>
 #include <string>
+#include <initializer_list>
 
-namespace Data_Structures
+namespace DataStructures
 {
-    template<unsigned int S, typename V>
-    class Hash_Table
+    template<typename T, unsigned int S>
+    class HashTable
     {
-    private:
-        std::list<std::pair<std::string, V>> table[S];
-
-        unsigned int Hash_Function(std::string key)
+	private:
+        std::list<std::pair<std::string, T>> table[S];
+		
+	public:
+		HashTable()
+		{}
+		
+		HashTable(const std::string& key, const T& data)
+		{
+			Insert(key, data);
+		}
+		
+		HashTable(const std::initializer_list<std::pair<std::string, T>>& list)
+		{
+			for(auto& pair : list)
+			{
+				Insert(pair.first, pair.second);
+			}
+		}
+		
+		~HashTable()
+		{}
+		
+        void Insert(const std::string& key, const T& data)
         {
-            unsigned int hash_key = 0;
+            unsigned int hash = HashFunction(key);
 
-            for(const char& chr : key)
+            for(auto& pair : table[hash])
             {
-                hash_key += (unsigned int)chr;
+				if(pair.first == key) {pair.second = data; return;}
             }
-
-            return hash_key % S;
+			
+			table[hash].emplace_back(key, data);
         }
 
-    public:
-        void Insert(std::string key, V value)
+        T GetData(const std::string& key)
         {
-            bool key_found = false;
-            unsigned int hash_key = Hash_Function(key);
+            unsigned int hash = HashFunction(key);
 
-            for(auto& key_value : table[hash_key])
+            for(auto& pair : table[hash])
             {
-                if(key_value.first == key)
-                {
-                    key_found = true;
-                    key_value.second = value;
-                }
-            }
-
-            if(key_found == false)
-            {
-                table[hash_key].emplace_back(key, value);
-            }
-        }
-
-        V Search(std::string key)
-        {
-            unsigned int hash_key = Hash_Function(key);
-
-            for(auto& key_value : table[hash_key])
-            {
-                if(key_value.first == key)
-                {
-                    return key_value.second;
-                }
+                if(pair.first == key) return pair.second;
             }
 
             return {};
         }
 
-        void Remove(std::string key)
+        void Remove(const std::string& key)
         {
-            unsigned int hash_key = Hash_Function(key);
-            auto& cell = table[hash_key];
-
-            for(auto i = cell.begin(); i != cell.end(); ++i)
+            unsigned int hash = HashFunction(key);
+			
+            for(auto& pair : table[hash])
             {
-                if(i->first == key)
-                {
-                    cell.erase(i);
-                    break;
-                }
+                if(pair.first == key) {table[hash].remove(pair); return;}
             }
         }
 
         bool Empty()
         {
-            unsigned int total = 0;
-
-            for(unsigned int i = 0; i < S; ++i)
+            for(unsigned int i = 0; i < S; i++)
             {
-                total += (unsigned int)table[i].size();
+                if(table[i].size() > 0) return false;
             }
 
-            return !total;
+            return true;
+        }
+		
+	private:
+        unsigned int HashFunction(const std::string& key)
+        {
+            unsigned int hash = 0;
+
+            for(const char& chr : key)
+            {
+                hash += (unsigned int)chr;
+            }
+
+            return hash % S;
         }
     };
 }
